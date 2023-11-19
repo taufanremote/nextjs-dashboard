1894cb4430ea10a36bd2b7cf7d70ea47c3601d4f
Taufan Prasetyo <taufanremote@gmail.com>
2023-11-19 19:55:48 +0700

    Layouts and Pages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const CustomerPage = (props: Props) => {
+  return <div>CustomerPage</div>;
+};
+
+export default CustomerPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const InvoicePage = (props: Props) => {
+  return <div>InvoicePage</div>;
+};
+
+export default InvoicePage;
