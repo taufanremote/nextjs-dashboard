15001dc1929cb934e722fe09cf58d40237227148
Taufan Prasetyo <taufanremote@gmail.com>
2023-11-19 19:18:47 +0700

    Optimizing Images

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 
 import { lusitana } from '@/app/ui/fonts';
 import styles from '@/app/ui/home.module.css';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -35,6 +36,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={700}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
