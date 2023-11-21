ea454d487e91eccd29d001184a8100d040d5e16e
Taufan Prasetyo <taufanremote@gmail.com>
2023-11-21 16:32:48 +0700

    Fethcing data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import { lusitana } from '@/app/ui/fonts';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { fetchRevenue } from '@/app/lib/data';
 
 type Props = {};
 
-const DashboardPage = (props: Props) => {
-  return <div>DashboardPage</div>;
+const DashboardPage = async (props: Props) => {
+  const revenue = await fetchRevenue();
+  console.log(revenue);
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
+  );
 };
 
 export default DashboardPage;
